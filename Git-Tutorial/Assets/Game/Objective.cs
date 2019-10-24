dd9af4036494d16f75a995b722b6700cdc50f0c7
Nicholas Chua <nicholaschuayunzhi@gmail.com>
2019-10-24 17:46:46 +0800

    Initial commit

---
+++ Git-Tutorial/Assets/Game/Objective.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Objective : MonoBehaviour
+{
+    public UnityEvent OnWin;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Win");
+            OnWin.Invoke();
+        }
+    }
+}
